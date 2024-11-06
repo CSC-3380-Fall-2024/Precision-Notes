4b1dfd0eaa45d89cfcdf53d08ad066077244fa20
simoomAmy <156845038+simoomAmy@users.noreply.github.com>
2024-11-05 20:03:39 -0600

    test

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import android.app.role.RoleManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.os.Build
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.RequiresApi
+\
+class MainActivity() : ComponentActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+
+    }
+
+
+}
+class DrawingView(context: Context, attrs:AttributeSet? = null): View(context, attrs){
+    private var path = Path()
+    private var paint = Paint().apply(){
+        color = Color.BLACK
+        strokeWidth = 5f
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+    private lateinit var bitmap: Bitmap
+    private lateinit var canvas: Canvas
+
+    init {
+        setBackgroundColor(Color.WHITE)
+    }
+    override fun onDraw(canvas: Canvas){
+        super.onDraw(canvas)
+        canvas.drawBitmap(bitmap, 0f, 0f, null)
+        canvas.drawPath(path, paint)
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> path.moveTo(event.x, event.y)
+            MotionEvent.ACTION_MOVE -> path.lineTo(event.x, event.y)
+            else -> return false
+        }
+    }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
